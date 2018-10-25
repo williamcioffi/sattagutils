7066969d84cb7e2a7cb221ca26ada65365c246cc
williamcioffi <william.cioffi@gmail.com>
2018-10-25 00:47:38 -0400

    changed sattag_show to show the filenames

--- R/sattag_show.R
+++ R/sattag_show.R
@@ -1,19 +1,19 @@
 #' @include sattag.R
+#' @include num2date.R
 NULL
 
 #' display an S4 object of class \code{\link[sattagutils]{sattag}}
 
 setMethod("show", "sattag", 
 	function(object) {
-		streams <- names(object)
-		streamtypes <- streamtype(object)
-		streamindices <- 1:length(streams)	
-		streams <- paste0(sprintf("%02d", streamindices), "- ", streams, " ", streamtypes)	
+		streamfnames <- filename(object)
+		streamindices <- 1:length(streamfnames)	
+		streams <- paste0(sprintf("%02d", streamindices), "- ", streamfnames)	
 		streams <- paste(streams, collapse = "\n")
 		cat(paste0("sattag type: ", object@instrument, "\n"))
 		cat(paste0("species: ", object@species, "\n"))
 		cat(paste0("deploy id: ", object@DeployID, "\nptt: ", object@Ptt, "\n"))
-		cat(paste0("start data date: ", object@t_start, "\nend data date: ", object@t_end, "\n"))
+		cat(paste0("start data date: ", format(num2date(object@t_start), "%Y-%m-%d"), "\nend data date: ", format(num2date(object@t_end), format = "%Y-%m-%d"), "\n"))
 		
 		cat("------\n") 
 		cat(paste0("streams: \n", streams, "\n"))
