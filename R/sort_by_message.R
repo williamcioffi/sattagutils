269139127fa3178e0c30d7cbd4b35bc3e1181d6b
williamcioffi <william.cioffi@gmail.com>
2019-01-12 19:06:38 -0500

    add sort_by_message function

---
+++ R/sort_by_message.R
@@ -0,0 +1,29 @@
+#' sort behavior streams
+#' 
+#' sort behavior streams by start time keeping messages together
+#' @param beh a beavior sattagstream.
+#' @return a behavior sattagstream sorted.
+#' @export
+
+sort_by_message <- function(beh) {
+  if(streamtype(beh) != "behavior") stop("beh must be a behavior sattagstream...")
+  
+  msgid <- cumsum(beh$What == "Message")
+  nmsg <- length(unique(umsgid))
+  
+  msgorder <- order(beh$Start[beh$What == "Message"])
+  runlen <- rle(msgid)$lengths
+  
+  msgsort <- vector("list", length = nmsg)
+  
+  k <- 1
+  for(i in 1:length(runlen)) {
+    st <- k
+    k <- k + runlen[i]
+    en <- k - 1
+    msgsort[[i]] <- st:en
+  }
+  
+  oo <- unlist(msgsort[msgorder])
+  beh[oo, ]
+}
